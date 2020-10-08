c18df7678676e260fca3e1d5a57591cf42a547af
makniahmed-31 <makni.ahmed@gmail.com>
2020-10-08 20:46:55 +0100

    Updates

---
+++ precache-manifest.c82f93475af68c059038ab06df13bd2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba64549cb07e3f10f824acf39cda0551",
+    "url": "/React-Router-Checkpoint/index.html"
+  },
+  {
+    "revision": "c9f89ea8461cefc1359d",
+    "url": "/React-Router-Checkpoint/static/css/main.a5ed603f.chunk.css"
+  },
+  {
+    "revision": "21a802f8908ab9e289ec",
+    "url": "/React-Router-Checkpoint/static/js/2.6e788239.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Router-Checkpoint/static/js/2.6e788239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9f89ea8461cefc1359d",
+    "url": "/React-Router-Checkpoint/static/js/main.b6b871ee.chunk.js"
+  },
+  {
+    "revision": "7d9e7a17022789e3ebed",
+    "url": "/React-Router-Checkpoint/static/js/runtime-main.cbe8fce7.js"
+  }
+]);
